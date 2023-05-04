301c4277b15086796ac316953b3b65f535b4482c
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-05-04 19:43:44 +0200

    added routing

--- src/app/access/access.module.ts
+++ src/app/access/access.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { AccessComponent } from './access.component';
+
+const routes: Routes = [{ path: '', component: AccessComponent }];
 
 @NgModule({
   declarations: [AccessComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class AccessModule {}
