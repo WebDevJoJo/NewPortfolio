a00e521af5df17f49908e011be4abc7767b4554f
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-05-04 19:11:07 +0200

    added path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'contacts', component: ContactsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
