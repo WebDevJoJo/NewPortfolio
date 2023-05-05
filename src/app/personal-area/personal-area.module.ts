3cedbff1c1c6d25e1f88ffd041d628b9f66628b0
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-05-05 09:40:47 +0200

    welcome component added

--- src/app/personal-area/personal-area.module.ts
+++ src/app/personal-area/personal-area.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { PreventiveComponent } from './preventive/preventive.component';
 import { PaymentMethodsComponent } from './payment-methods/payment-methods.component';
 import { PartnerCompaniesComponent } from './partner-companies/partner-companies.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     PreventiveComponent,
     PaymentMethodsComponent,
     PartnerCompaniesComponent,
+    WelcomeComponent,
   ],
   imports: [CommonModule],
 })
