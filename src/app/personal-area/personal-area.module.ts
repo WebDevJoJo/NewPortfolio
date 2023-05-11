09caf3aea1401b26496b82ea19e63917a4ec7e8e
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-05-11 17:25:19 +0200

    added matlist module

--- src/app/personal-area/personal-area.module.ts
+++ src/app/personal-area/personal-area.module.ts
@@ -10,6 +10,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatListModule } from '@angular/material/list';
 
 import { WelcomeComponent } from './welcome/welcome.component';
 import { PreventiveComponent } from './preventive/preventive.component';
@@ -47,6 +48,7 @@ const routes: Routes = [
     MatIconModule,
     MatButtonModule,
     MatDialogModule,
+    MatListModule,
   ],
 })
 export class PersonalAreaModule {}
